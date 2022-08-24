702a3dce3bce3d22b5abf44722dce4254cf3d0db
astr0n0mer <imran2000786@gmail.com>
2022-08-25 00:56:15 +0530

    Added dynamic board creation functionality

--- app.js
+++ app.js
@@ -17,13 +17,40 @@ var symbols = [
 ];
 
 let symbolDropdowns = document.getElementsByClassName("input-symbol");
+var startButton = document.getElementById("start-game");
+var mainMenuButton = document.getElementById("main-menu");
 var playerNames = [];
 var playerSymbols = [];
+
+function initialSymbolDataLoad() {
+  for (let sd of symbolDropdowns) sd.innerHTML = "";
+  for (let i = 0; i < symbolDropdowns.length; i++)
+    for (let j = 0; j < symbols.length; j++)
+      if (i == j || j >= symbolDropdowns.length) {
+        let option = document.createElement("option");
+        option.text = option.value = symbols[j];
+        symbolDropdowns[i].appendChild(option);
+      }
+}
 
 function updatePlayerSymbols() {
   playerSymbols = [];
   for (let sd of symbolDropdowns)
     playerSymbols.push(sd.children[sd.selectedIndex].text);
+}
+
+function createBoard(n) {
+  let board = document.getElementById("board");
+  board.innerHTML = "";
+  for (let i = 0; i <= n; i++) {
+    let boardRow = document.createElement("span");
+    boardRow.classList.add("board-row");
+    for (let j = 0; j <= n; j++) {
+      let button = document.createElement("button");
+      boardRow.appendChild(button);
+    }
+    board.appendChild(boardRow);
+  }
 }
 
 for (let sd of symbolDropdowns) {
@@ -41,43 +68,28 @@ for (let sd of symbolDropdowns) {
         for (let op of symbolDropdowns[i].children)
           if (op.outerHTML == selectedOption.outerHTML)
             symbolDropdowns[i].removeChild(op);
-        // for (let option of symbolDropdowns[i].children)
-        //   if (option.text == selectedOption.text)
-        //     symbolDropdowns[i].removeChild(option);
       }
     }
     updatePlayerSymbols();
   };
 }
 
-function initialSymbolDataLoad() {
-  for (let sd of symbolDropdowns) sd.innerHTML = "";
-  for (let i = 0; i < symbolDropdowns.length; i++) {
-    for (let j = 0; j < symbols.length; j++) {
-      if (i == j || j >= symbolDropdowns.length) {
-        let option = document.createElement("option");
-        option.text = option.value = symbols[j];
-        symbolDropdowns[i].appendChild(option);
-      }
-    }
-  }
-}
-
-// function updateSymbolDropdowns(symbolDropdowns) {
-//   let usedSymbols = [];
-//   for (let sd in symbolDropdowns)
-//     if (sd.childElementCount > 0)
-//       usedSymbols.push(sd.children[sd.selectedIndex].text);
-// }
-window.onload = () => {
-  initialSymbolDataLoad();
-  updatePlayerSymbols();
-  //   for (let sd of symbolDropdowns) {
-  //     sd.innerHTML = "";
-  //     for (let j = 0; j < symbols.length; j++) {
-  //       let option = document.createElement("option");
-  //       option.textContent = symbols[j];
-  //       sd.appendChild(option);
-  //     }
-  //   }
+startButton.onclick = () => {
+  window.scroll({
+    top: document.body.scrollHeight,
+    left: 0,
+    behavior: "smooth",
+  });
+  createBoard(symbolDropdowns.length);
 };
+
+mainMenuButton.onclick = () => {
+  window.scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
+
+initialSymbolDataLoad();
+updatePlayerSymbols();
